fbeae716dcf6abc0b3488d406ad644daa528daec
Dmitry Baryshnikov <bishop.dev@gmail.com>
2016-03-18 04:03:44 +0300

    Update configure header

--- cmake/configure.cmake
+++ cmake/configure.cmake
@@ -3,7 +3,7 @@
 # Purpose:  CMake build scripts
 # Author:   Dmitry Baryshnikov, polimax@mail.ru
 ################################################################################
-# Copyright (C) 2015, NextGIS <info@nextgis.com>
+# Copyright (C) 2015-2016, NextGIS <info@nextgis.com>
 # Copyright (C) 2012,2013,2014 Dmitry Baryshnikov
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
