70be0b96adf15951ece2b7d148dcd4e264d90b65
Dmitry Baryshnikov <bishop.dev@gmail.com>
2016-12-25 23:04:25 +0300

    Add package repository support

--- cmake/FindGEOS.cmake
+++ cmake/FindGEOS.cmake
@@ -12,6 +12,17 @@
 #    GEOS_INCLUDE_DIR
 #    GEOS_LIBRARY
 #
+
+FUNCTION (GET_VERSION_PLIST PLISTFILE OUTVAR)
+	SET (PVERSION "")
+	IF (EXISTS ${PLISTFILE})
+		FILE (READ "${PLISTFILE}" info_plist)
+		STRING (REGEX REPLACE "\n" "" info_plist "${info_plist}")
+		STRING (REGEX MATCH "<key>CFBundleShortVersionString</key>[ \t]*<string>([0-9\\.]*)</string>" PLISTVERSION "${info_plist}")
+		STRING (REGEX REPLACE "<key>CFBundleShortVersionString</key>[ \t]*<string>([0-9\\.]*)</string>" "\\1" PVERSION "${PLISTVERSION}")
+	ENDIF (EXISTS ${PLISTFILE})
+	SET (${OUTVAR} ${PVERSION} PARENT_SCOPE)
+ENDFUNCTION (GET_VERSION_PLIST)
  
 IF(WIN32)
 
@@ -22,7 +33,7 @@ IF(WIN32)
 
   IF (MSVC)
     FIND_PATH(GEOS_INCLUDE_DIR geos_c.h $ENV{LIB_DIR}/include $ENV{INCLUDE})
-    FIND_LIBRARY(GEOS_LIBRARY NAMES geos geos_c_i PATHS 
+    FIND_LIBRARY(GEOS_LIBRARY NAMES geos geos_c_i PATHS
       "$ENV{LIB}/lib"
       $ENV{LIB}
       #mingw
@@ -31,15 +42,15 @@ IF(WIN32)
       )
     IF (GEOS_LIBRARY)
        SET (
-         GEOS_LIBRARY 
-         GEOS_LIBRARY;odbc32;odbccp32 
+         GEOS_LIBRARY
+         GEOS_LIBRARY;odbc32;odbccp32
          CACHE STRING INTERNAL)
     ENDIF (GEOS_LIBRARY)
   ENDIF (MSVC)
-  
+
 ELSE(WIN32)
 
- IF(UNIX) 
+ IF(UNIX)
 
     # try to use framework on mac
     # want clean framework path, not unix compatibility path
@@ -78,7 +89,7 @@ ELSE(WIN32)
       #MESSAGE("DBG GEOS_CONFIG ${GEOS_CONFIG}")
 
       IF (GEOS_CONFIG)
-      
+
         EXEC_PROGRAM(${GEOS_CONFIG}
             ARGS --version
             OUTPUT_VARIABLE GEOS_VERSION)
@@ -90,14 +101,14 @@ ELSE(WIN32)
             ARGS --prefix
             OUTPUT_VARIABLE GEOS_PREFIX)
 
-        FIND_PATH(GEOS_INCLUDE_DIR 
-            geos_c.h 
+        FIND_PATH(GEOS_INCLUDE_DIR
+            geos_c.h
             ${GEOS_PREFIX}/include
-            /usr/local/include 
-            /usr/include 
+            /usr/local/include
+            /usr/include
             )
 
-        ## extract link dirs for rpath  
+        ## extract link dirs for rpath
         EXEC_PROGRAM(${GEOS_CONFIG}
             ARGS --libs
             OUTPUT_VARIABLE GEOS_CONFIG_LIBS )
@@ -105,13 +116,13 @@ ELSE(WIN32)
         ## split off the link dirs (for rpath)
         ## use regular expression to match wildcard equivalent "-L*<endchar>"
         ## with <endchar> is a space or a semicolon
-        STRING(REGEX MATCHALL "[-][L]([^ ;])+" 
-            GEOS_LINK_DIRECTORIES_WITH_PREFIX 
+        STRING(REGEX MATCHALL "[-][L]([^ ;])+"
+            GEOS_LINK_DIRECTORIES_WITH_PREFIX
             "${GEOS_CONFIG_LIBS}" )
         #MESSAGE("DBG  GEOS_LINK_DIRECTORIES_WITH_PREFIX=${GEOS_LINK_DIRECTORIES_WITH_PREFIX}")
 
         ## remove prefix -L because we need the pure directory for LINK_DIRECTORIES
-      
+
         IF (GEOS_LINK_DIRECTORIES_WITH_PREFIX)
           STRING(REGEX REPLACE "[-][L]" "" GEOS_LINK_DIRECTORIES ${GEOS_LINK_DIRECTORIES_WITH_PREFIX} )
         ENDIF (GEOS_LINK_DIRECTORIES_WITH_PREFIX)
@@ -120,15 +131,15 @@ ELSE(WIN32)
         ## split off the name
         ## use regular expression to match wildcard equivalent "-l*<endchar>"
         ## with <endchar> is a space or a semicolon
-        #STRING(REGEX MATCHALL "[-][l]([^ ;])+" 
-        #  GEOS_LIB_NAME_WITH_PREFIX 
+        #STRING(REGEX MATCHALL "[-][l]([^ ;])+"
+        #  GEOS_LIB_NAME_WITH_PREFIX
         #  "${GEOS_CONFIG_LIBS}" )
         #MESSAGE("DBG  GEOS_CONFIG_LIBS=${GEOS_CONFIG_LIBS}")
         #MESSAGE("DBG  GEOS_LIB_NAME_WITH_PREFIX=${GEOS_LIB_NAME_WITH_PREFIX}")
         SET(GEOS_LIB_NAME_WITH_PREFIX -lgeos_c CACHE STRING INTERNAL)
 
         ## remove prefix -l because we need the pure name
-      
+
         IF (GEOS_LIB_NAME_WITH_PREFIX)
           STRING(REGEX REPLACE "[-][l]" "" GEOS_LIB_NAME ${GEOS_LIB_NAME_WITH_PREFIX} )
         ENDIF (GEOS_LIB_NAME_WITH_PREFIX)
@@ -145,7 +156,7 @@ ELSE(WIN32)
           SET(GEOS_LIBRARY ${GEOS_LINK_DIRECTORIES}/lib${GEOS_LIB_NAME}.so CACHE STRING INTERNAL)
         ENDIF (APPLE)
         #MESSAGE("DBG  GEOS_LIBRARY=${GEOS_LIBRARY}")
-      
+
       ELSE(GEOS_CONFIG)
         MESSAGE("FindGEOS.cmake: geos-config not found. Please set it manually. GEOS_CONFIG=${GEOS_CONFIG}")
       ENDIF(GEOS_CONFIG)
@@ -156,8 +167,8 @@ ENDIF(WIN32)
 # Handle the QUIETLY and REQUIRED arguments and set GEOS_FOUND to TRUE
 # if all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GEOS 
-                                  REQUIRED_VARS GEOS_LIBRARY GEOS_INCLUDE_DIR 
+find_package_handle_standard_args(GEOS
+                                  REQUIRED_VARS GEOS_LIBRARY GEOS_INCLUDE_DIR
                                   VERSION_VAR GEOS_VERSION)
 
 # Hide internal variables
