1960f5466afa0838908a835ffe6e397719ddd008
d3n15 <x514ckw4r3@gmail.com>
2022-06-20 15:27:59 +0300

    Added test for creating/reading SXF data

--- tests/ogr/ogr_sxf.py
+++ tests/ogr/ogr_sxf.py
@@ -32,7 +32,9 @@ import sys
 import gdaltest
 from osgeo import gdal
 from osgeo import ogr
+from osgeo import osr
 import pytest
+import random
 
 ###############################################################################
 # Open SXF datasource.
@@ -120,6 +122,72 @@ def test_ogr_sxf_4(capsys):
             pytest.skip('skipping test: recode is not possible')
         assert lyr_names[layer_n] == lyr.GetName()
 
+###############################################################################
+# Create/Read SXF datasource
+
+def create_datasource(sxf_name, test_data, layer_names):
+
+    create_ds  = gdal.GetDriverByName('SXF').Create(sxf_name, 0, 0, 0, gdal.GDT_Unknown)
+    assert create_ds is not None
+
+    for name in layer_names:
+        layer = create_ds.CreateLayer(name, srs = osr.SpatialReference(), geom_type = ogr.wkbPoint, options = ['SXF_NEW_BEHAVIOR=YES'])
+        assert layer is not None
+        
+        layer.CreateField(ogr.FieldDefn('STRFIELD', ogr.OFTString))
+        layer.CreateField(ogr.FieldDefn('DECFIELD', ogr.OFTInteger))
+        layer.CreateField(ogr.FieldDefn('REALFIELD', ogr.OFTReal))
+        
+        feature = ogr.Feature(layer.GetLayerDefn())
+        
+        # With SXF_NEW_BEHAVIOR=YES, features are not created if the some system fields are not filled in:
+        feature.SetField('OT', 'P')
+        feature.SetField('CLCODE', random.randint(0, 999))
+        
+        feature.SetField('STRFIELD', test_data['STRFIELD_TEST_VALUE'])
+        feature.SetField('DECFIELD', test_data['DECFIELD_TEST_VALUE'])
+        feature.SetField('REALFIELD', test_data['REALFIELD_TEST_VALUE'])
+        feature.SetGeometry(ogr.CreateGeometryFromWkt('POINT(0 0)'))
+        
+        ret = layer.CreateFeature(feature)
+        assert ret == 0 and feature.GetFID() >= 0
+
+def read_datasource(sxf_name, test_data, layer_names):
+
+    open_ds = gdal.OpenEx(sxf_name, gdal.OF_READONLY, open_options = ['SXF_NEW_BEHAVIOR=YES'])
+    assert open_ds is not None
+    assert open_ds.GetLayerCount() == len(layer_names) + 2 # SYSTEM and Not_Classified
+
+    for i in range(open_ds.GetLayerCount()):
+        layer = open_ds.GetLayer(i)
+        if layer.GetName() not in ['SYSTEM', 'Not_Classified']:
+            assert layer.GetName() in layer_names
+            
+            feature = layer.GetNextFeature()
+            assert feature is not None
+            
+            for i in range(feature.GetFieldCount()):
+                field = feature.GetFieldDefnRef(i)
+                assert field is not None
+                
+                if field.GetAlternativeName() == 'STRFIELD':
+                    assert feature.GetFieldAsString(i) == test_data['STRFIELD_TEST_VALUE']
+                elif field.GetAlternativeName() == 'DECFIELD':
+                    assert feature.GetFieldAsInteger(i) == test_data['DECFIELD_TEST_VALUE']
+                elif field.GetAlternativeName() == 'REALFIELD':                    
+                    assert feature.GetFieldAsDouble(i) == test_data['REALFIELD_TEST_VALUE']
+
+def test_ogr_sxf_5():
+    sxf_name = 'tmp/test_ogr_sxf_5.sxf'
+    layer_names = ['ВОДНЫЕ ОБЪЕКТЫ', 'ИНФРАСТРУКТУРА']
+    test_data = {
+        'STRFIELD_TEST_VALUE': '123',
+        'DECFIELD_TEST_VALUE': 123,
+        'REALFIELD_TEST_VALUE': 1.23,
+    }
+    
+    create_datasource(sxf_name, test_data, layer_names)
+    read_datasource(sxf_name, test_data, layer_names)
 
 ###############################################################################
 #
